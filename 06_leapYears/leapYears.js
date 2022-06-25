8d94a1acfff082c713c701e9c2ea5a1c7bb70a7d
nameless-bastard <wewlococoz@gmail.com>
2022-06-25 15:17:11 +0800

    Update: finished exercise for 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0){
+        if(year % 100 === 0 && !(year % 400 === 0)) return year = false;
+        return year = true;
+    } else {
+        return year = false;
+    }
 };
 
 // Do not edit below this line
