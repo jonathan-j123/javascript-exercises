cd8d9f3efacdce8833bd2e14b8677edab95c381d
nameless-bastard <wewlococoz@gmail.com>
2022-06-25 13:09:27 +0800

    Update:  All condition are met

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(firstNo, secondNo ) {
+    let i = firstNo;
+    let repetition = secondNo;
+    let sum = 0;
+    if(typeof firstNo !== 'number' || typeof secondNo !== 'number') return "ERROR";
+    if(firstNo < 0 || secondNo < 0) return "ERROR";
+    if(firstNo > secondNo) {
+        i = secondNo; 
+        repetition = firstNo;
 
+    }
+    for (; i <= repetition; i++){
+        
+        sum += i; 
+    }
+    return sum;
 };
 
 // Do not edit below this line
